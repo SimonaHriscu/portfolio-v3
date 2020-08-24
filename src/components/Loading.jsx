a67aa55c3d6598fcba7ed16c5bdf7350720ef074
Simona <see.moona@yahoo.com>
2020-08-24 18:03:30 +0200

    new components

---
+++ src/components/Loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Loading() {
+  return (
+    <div className="loading">
+      <img src="/static/media/loader.534b1a10.gif" alt="logo" />
+    </div>
+  );
+}
+
+export default Loading;
---
+++ src/components/Main/MainContainer.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function MainContainer() {
+  return <div className="main-container"></div>;
+}
+
+export default MainContainer;
