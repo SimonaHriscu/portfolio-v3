fb882034c4346b8333b6f813632e4723720403b7
Simona <see.moona@yahoo.com>
2020-08-24 18:02:57 +0200

    new component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import Loading from './components/Loading';
+import MainContainer from './components/Main/MainContainer';
 
-function App() {
-  return <div className="App"></div>;
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+    };
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        loading: false,
+      });
+    }, 5000);
+  }
+  render() {
+    if (this.state.loading) return <Loading />;
+
+    return (
+      <div className="App">
+        <MainContainer />;
+      </div>
+    );
+  }
 }
-
-export default App;
