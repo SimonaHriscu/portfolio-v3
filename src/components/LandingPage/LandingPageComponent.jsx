a9f83740e1eb18fc324ddef66f790a8a1cde956a
Simona <see.moona@yahoo.com>
2020-08-24 19:37:57 +0200

    landing transitions added

---
+++ src/components/LandingPage/LandingPageComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function LandingInfo() {
+  return (
+    <div className="landing-info">
+      <p className="name-container">HI this is Simona</p>
+      <p className="name-container">HI this is Simona</p>
+      <p className="name-container">HI this is Simona</p>
+      <p className="name-container">HI this is Simona</p>
+    </div>
+  );
+}
+
+export default LandingInfo;
--- src/components/Main/MainContainer.jsx
+++ src/components/Main/MainContainer.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import LandingInfo from '../LandingPage/LandingPageComponent';
 
 function MainContainer() {
-  return <div className="main-container"></div>;
+  return (
+    <div className="main-container">
+      <LandingInfo />
+    </div>
+  );
 }
 
 export default MainContainer;
