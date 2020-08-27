5d25e05e7fdf84e303fc11bb270995732d20d08d
Simona <see.moona@yahoo.com>
2020-08-27 10:53:45 +0200

    contact finished

--- src/components/Main/LandingPage/Contact/ContactComponent.jsx
+++ src/components/Main/LandingPage/Contact/ContactComponent.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
 
 function Contact() {
-  return <div className="contact">This is CONTACT</div>;
+  return (
+    <div className="contact">
+      <ul>
+        <li>get in touch</li>
+        <li>
+          <div></div>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/simona-hriscu/" target="_black">
+            <ion-icon name="logo-linkedin"></ion-icon>
+          </a>{' '}
+        </li>
+        <li>
+          <a href="https://github.com/SimonaHriscu" target="_black">
+            {' '}
+            <ion-icon name="logo-github"></ion-icon>
+          </a>
+        </li>
+        <li>
+          <a href="mailto:simona_hriscu@yahoo.com?Subject=Hello">
+            <ion-icon name="mail"></ion-icon>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Contact;
