334eb7b8f1a26a26e004b1d16b2743106b9c00b7
Simona <see.moona@yahoo.com>
2020-08-28 00:21:16 +0200

    onClick does not work yet

--- src/components/Main/LandingPage/Nav/NavComponent.jsx
+++ src/components/Main/LandingPage/Nav/NavComponent.jsx
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const menuItems = ['about me', 'skills', 'portfolio', 'contact'];
 const links = ['#landing-page', '#section_2', '#section_3', '#section_4'];
-function Nav() {
+
+const Nav = () => {
+  const [className, setClassName] = useState('');
+
+  // const newClass = class;
+
+  const updateClass = (itemToChange, key) => {
+    const newArr = menuItems.map((item, i) => console.log(item));
+
+    //setColor(menuItems.includes(itemToChange) ? 'red' : 'white');
+    setClassName(menuItems.includes(itemToChange) ? 'newClass' : '');
+  };
   return (
     <div className="nav">
       <ul>
         {menuItems.map((menuItem, i) => (
-          <a href={links[i]}>
-            {' '}
-            <li>{menuItem}</li>
+          <a href={links[i]} key={i} className={className}>
+            <li onClick={() => updateClass(menuItem, key)}>{menuItem}</li>
           </a>
         ))}
       </ul>
     </div>
   );
-}
+};
 
 export default Nav;
