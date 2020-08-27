2b2163ce6d2037799148aa10c7f28085c7f8f584
Simona <see.moona@yahoo.com>
2020-08-27 11:59:30 +0200

    new sections

--- src/components/Main/LandingPage/Info/InfoComponent.jsx
+++ src/components/Main/LandingPage/Info/InfoComponent.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 function Info() {
   return (
     <div className="landing-info">
-      <p className="name-container">HI this is Simona</p>
-      <p className="name-container">HI this is Simona</p>
-      <p className="name-container">HI this is Simona</p>
-      <p className="name-container">HI this is Simona</p>
+      <section id={'section_1'}>THis is section 1</section>
+      <section id={'section_2'}>This is section 2</section>
+      <section id={'section_3'}>This is section 3</section>
+      <section id={'section_4'}>This is section 4</section>
     </div>
   );
 }
