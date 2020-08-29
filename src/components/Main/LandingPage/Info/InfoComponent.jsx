66c9e029e93d2cf5e74348c67c9147eb7eaf4509
Simona <see.moona@yahoo.com>
2020-08-29 22:15:41 +0200

    extra skills added

--- src/components/Main/LandingPage/Info/InfoComponent.jsx
+++ src/components/Main/LandingPage/Info/InfoComponent.jsx
@@ -22,7 +22,7 @@ function Info() {
         <div className="skills-section">
           {' '}
           <h1>Skills</h1>
-          <p>
+          <p className="info-p">
             I have a broad range of applicable skills that will enable me to be
             a valuable member of your team. These include specific technical
             skills I have developed over the past year along with strong soft
@@ -31,61 +31,74 @@ function Info() {
           <ul className="skills-gallery">
             <li>
               <ion-icon name="logo-html5"></ion-icon>
-              <p>HTML</p>
+              <p
+                className="skills-gallery__title"
+                className="skills-gallery__title"
+              >
+                {' '}
+                HTML
+              </p>
             </li>
             <li>
               <ion-icon name="logo-css3"></ion-icon>
-              <p>CSS</p>
+              <p className="skills-gallery__title">CSS</p>
             </li>
             <li>
               <ion-icon name="logo-sass"></ion-icon>
-              <p>SASS</p>
+              <p className="skills-gallery__title">SASS</p>
             </li>
             <li>
               <ion-icon name="logo-react"></ion-icon>
-              <p>React</p>
+              <p className="skills-gallery__title">React</p>
             </li>
             <li>
               <ion-icon name="logo-javascript"></ion-icon>
-              <p>JavaScript</p>
+              <p className="skills-gallery__title">JavaScript</p>
             </li>
             <li>
               <ion-icon name="logo-nodejs"></ion-icon>
-              <p>Node.js</p>
+              <p className="skills-gallery__title">Node.js</p>
             </li>
             <li>
               <ion-icon name="logo-npm"></ion-icon>
-              <p>NPM</p>
+              <p className="skills-gallery__title">NPM</p>
             </li>
             <li>
               <ion-icon name="logo-github"></ion-icon>
-              <p>GitHub</p>
+              <p className="skills-gallery__title">GitHub</p>
             </li>
           </ul>
-          <p>I have become familial with these technologies as well:</p>
+          <p>
+            I have become familial with and can successfully use these
+            technologies as well:
+          </p>
           <ul className="extra-skills">
             <li>
-              <ion-icon name="arrow-redo-outline"></ion-icon>
+              <ion-icon name="add-outline"></ion-icon>
               <span>Bootstrap</span>
             </li>
             <li>
-              <ion-icon name="arrow-redo-outline"></ion-icon>
+              <ion-icon name="add-outline"></ion-icon>
               <span>Redux</span>
             </li>
             <li>
-              <ion-icon name="arrow-redo-outline"></ion-icon>
+              <ion-icon name="add-outline"></ion-icon>
               <span>React Hooks</span>
             </li>
             <li>
-              <ion-icon name="arrow-redo-outline"></ion-icon>
+              <ion-icon name="add-outline"></ion-icon>
+              <span>AJAX/Fetch API</span>
+            </li>
+            <li>
+              <ion-icon name="add-outline"></ion-icon>
               <span>MongoDB</span>
             </li>
             <li>
-              <ion-icon name="arrow-redo-outline"></ion-icon>
+              <ion-icon name="add-outline"></ion-icon>
               <span>Express.js</span>
             </li>
             <li>
-              <ion-icon name="arrow-redo-outline"></ion-icon>
+              <ion-icon name="add-outline"></ion-icon>
               <span>Mongoose</span>
             </li>
           </ul>
