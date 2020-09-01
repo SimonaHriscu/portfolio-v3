eb9694a987a841c978313b1e0006d41b25d39362
Simona <see.moona@yahoo.com>
2020-09-01 08:13:19 +0200

    more space in the landing page

--- src/components/Main/LandingPage/Info/sections/Section1/Section1.jsx
+++ src/components/Main/LandingPage/Info/sections/Section1/Section1.jsx
@@ -5,18 +5,18 @@ function Section1() {
   return (
     <section id={'section_1'}>
       <div className="profile-section">
-        <p>Hello, My name is Simona</p>
+        <p>Hello,</p>
+        <p>My name is Simona</p>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta atque
           deserunt vitae! Expedita quos incidunt asperiores quam deleniti saepe,
-          odio a velit animi placeat ipsam, quo, inventore nobis ea quae.
+          odio a velit animi placeat ipsam, quo, inventore nobis ea quae. Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Voluptate, nulla!
         </p>
-        <p>
-          <a href="#section_3">
-            {' '}
-            <button className="button.btn.fourth">Projects</button>
-          </a>
-        </p>
+        <a href="#section_3">
+          {' '}
+          <button className="button.btn.fourth">Projects</button>
+        </a>
       </div>
       <div className="profile-picture">
         <div className="profile-picture__container">
