fbded2a1e53842737a090cc0aa2faf78f4346e09
Simona <see.moona@yahoo.com>
2020-09-01 10:21:47 +0200

    working on animation

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Loading = (props) => {
-  console.log(props.gif);
+  //console.log(props.gif);
   return (
     <div className="loading">
       <img src={props.gif} alt="logo" />
