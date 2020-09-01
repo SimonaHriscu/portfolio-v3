f9d9c99feeb3db7c8f4024b0597b38275ae3e405
Simona <see.moona@yahoo.com>
2020-09-01 12:50:41 +0200

    working NAV

--- src/components/Main/LandingPage/Nav/NavComponent.jsx
+++ src/components/Main/LandingPage/Nav/NavComponent.jsx
@@ -1,44 +1,42 @@
 import React, { useState } from 'react';
 
-const menuItems = ['about me', 'skills', 'portfolio', 'contact'];
-const links = ['#landing-page', '#section_2', '#section_3', '#section_4'];
-
-const Nav = () => {
-  const [currentState, setNewState] = useState({
-    className: 'non-color',
-  });
-
-  let index = 0;
-
-  const updateName = (itemToChange, i) => {
-    console.log(itemToChange);
-    index = i;
-    console.log(index);
-
-    setNewState({
-      className: 'color',
-    });
+class Nav extends React.Component {
+  menuItems = [
+    { title: 'about me' },
+    { title: 'skills' },
+    { title: 'portfolio' },
+    { title: 'contact' },
+  ];
+  links = ['#landing-page', '#section_2', '#section_3', '#section_4'];
+  state = {
+    currentIndex: 0,
   };
 
-  return (
-    <div className="nav">
-      <ul>
-        {menuItems.map((menuItem, i) => (
-          <a href={links[i]}>
-            <li
-              key={i}
-              className={index === i ? 'color' : 'non-color'}
-              onClick={() => updateName(menuItem, i)}
-            >
-              {/* {i} */}
-              {menuItem}
-            </li>
-          </a>
-        ))}
-      </ul>
-    </div>
-  );
-};
+  updateIndex(i) {
+    this.setState({ currentIndex: i });
+  }
+  render() {
+    return (
+      <div className="nav">
+        <ul>
+          {this.menuItems.map((menuItem, i) => (
+            <a href={this.links[i]}>
+              <li
+                key={i}
+                onClick={() => this.updateIndex(i)}
+                className={
+                  this.state.currentIndex === i ? 'color' : 'not-color'
+                }
+              >
+                {menuItem.title}
+              </li>
+            </a>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default Nav;
 
