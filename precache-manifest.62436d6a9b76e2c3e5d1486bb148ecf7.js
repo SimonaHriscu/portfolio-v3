633111bdf6da1ac6c8732d32286744cc3f6ab9f3
Simona <see.moona@yahoo.com>
2020-09-01 12:51:26 +0200

    Updates

--- precache-manifest.62436d6a9b76e2c3e5d1486bb148ecf7.js
+++ precache-manifest.62436d6a9b76e2c3e5d1486bb148ecf7.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "609ad7a6433a3f5e96f21b8ba4bfe2c9",
+    "revision": "87ee14ab016719d4f61348964f5d98bc",
     "url": "/portfolio-v3/index.html"
   },
   {
-    "revision": "89c9cd92ec69ac62b978",
-    "url": "/portfolio-v3/static/css/main.7196073e.chunk.css"
+    "revision": "665adb8430cba5f4d758",
+    "url": "/portfolio-v3/static/css/main.cbf2bb7b.chunk.css"
   },
   {
-    "revision": "54e3d523fc373e71eaf0",
-    "url": "/portfolio-v3/static/js/2.e0cccc7d.chunk.js"
+    "revision": "7ad72ebc0164783df2a5",
+    "url": "/portfolio-v3/static/js/2.f2b91e75.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/portfolio-v3/static/js/2.e0cccc7d.chunk.js.LICENSE.txt"
+    "url": "/portfolio-v3/static/js/2.f2b91e75.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "89c9cd92ec69ac62b978",
-    "url": "/portfolio-v3/static/js/main.cf1a153b.chunk.js"
+    "revision": "665adb8430cba5f4d758",
+    "url": "/portfolio-v3/static/js/main.ad042edd.chunk.js"
   },
   {
     "revision": "b5eff4e927cdff1a68d8",
