ab87af3d03b8ad6a4c92f9c21def73566587b300
Simona <see.moona@yahoo.com>
2020-09-01 08:52:35 +0200

    Updates

---
+++ precache-manifest.15c41a38e423947b2b9911f2923237ae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15935186f66be5433fb0e5a9b9e65d61",
+    "url": "/portfolio-v3/index.html"
+  },
+  {
+    "revision": "3ba5833dfd8de8f3f251",
+    "url": "/portfolio-v3/static/css/main.7196073e.chunk.css"
+  },
+  {
+    "revision": "54e3d523fc373e71eaf0",
+    "url": "/portfolio-v3/static/js/2.e0cccc7d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-v3/static/js/2.e0cccc7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ba5833dfd8de8f3f251",
+    "url": "/portfolio-v3/static/js/main.4383daef.chunk.js"
+  },
+  {
+    "revision": "b5eff4e927cdff1a68d8",
+    "url": "/portfolio-v3/static/js/runtime-main.097b8d46.js"
+  },
+  {
+    "revision": "534b1a10490cdda462db7f7330a9fba3",
+    "url": "/portfolio-v3/static/media/loader.534b1a10.gif"
+  },
+  {
+    "revision": "564395c570b25dc7dee7b399e00da6bc",
+    "url": "/portfolio-v3/static/media/simona.564395c5.jpg"
+  }
+]);
