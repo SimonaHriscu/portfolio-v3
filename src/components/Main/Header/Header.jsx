59e692fb00d8cd9d84c70753c18eab7bd8ab049e
Simona <see.moona@yahoo.com>
2020-09-01 08:49:07 +0200

    header link added

--- src/components/Main/Header/Header.jsx
+++ src/components/Main/Header/Header.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 function Header() {
   return (
     <div className="header">
-      <p>S</p> <span>H</span>
+      <a href="#landing-page">
+        <p>S</p> <span>H</span>
+      </a>
     </div>
   );
 }
