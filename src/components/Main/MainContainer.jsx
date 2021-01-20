25fa6357db1e53fc42dc51b16b13c55c6cfe397c
SimonaHriscu <simona_hriscu@yahoo.com>
2021-01-20 23:34:43 +0100

    update main container

--- src/components/Main/MainContainer.jsx
+++ src/components/Main/MainContainer.jsx
@@ -1,26 +1,22 @@
 import React, { useState } from 'react';
 import Header from './Header/Header';
 import LandingPage from './LandingPage/LandingPageComponent';
-import SideDrawer from '../Main/LandingPage/Nav/SideDrawer';
+import SideDrawer from './LandingPage/Nav/SideDrawer';
 import Backdrop from './LandingPage/Nav/Backdrop';
 
-const MainContainer = (prop) => {
+const MainContainer = () => {
   const [drawerIsOpen, setDrawerIsOpen] = useState(false);
-
   const openDrawerHandler = () => {
     setDrawerIsOpen(!drawerIsOpen);
   };
-
   const closeDrawerHandler = () => {
     setDrawerIsOpen(false);
   };
   return (
     <div className="main-container">
       <Header open={openDrawerHandler} />
-      {/* && ====>  if this is true, render that */}
       {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
-      <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}></SideDrawer>
-
+      <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}/>
       <LandingPage />
     </div>
   );
