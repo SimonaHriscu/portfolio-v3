7124c0ebfed2a8a1f028ae6db050a64c98689cc4
SimonaHriscu <simona_hriscu@yahoo.com>
2021-01-20 23:12:56 +0100

    loading page refactored with hooks

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
-import React from 'react';
-import Loading from './components/Loading/Loading';
-import MainContainer from './components/Main/MainContainer';
-import Gif from './components/Main/img/loader.gif';
+import React, { useState, useEffect } from "react";
+import Loading from "./components/Loading/Loading";
+import MainContainer from "./components/Main/MainContainer";
+import Gif from "./components/Main/img/loader.gif";
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loading: true,
-    };
-  }
-  componentDidMount() {
+const App = () => {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
     setTimeout(() => {
-      this.setState({
-        loading: false,
-      });
+      setLoading(false);
     }, 4400);
-  }
-  render() {
-    if (this.state.loading) return <Loading gif={Gif} />;
-
-    return (
-      <div className="App">
-        <MainContainer />
-      </div>
-    );
-  }
-}
+  }, []);
+  return loading ? (
+    <Loading gif={Gif} />
+  ) : (
+    <div className="App">
+      <MainContainer />
+    </div>
+  );
+};
+export default App;
