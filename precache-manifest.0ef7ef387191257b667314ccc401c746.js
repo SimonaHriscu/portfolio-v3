ad5fe9bb768c572c52e6491955565f3d69bf7b96
SimonaHriscu <simona_hriscu@yahoo.com>
2021-01-21 00:11:15 +0100

    Updates

--- precache-manifest.0ef7ef387191257b667314ccc401c746.js
+++ precache-manifest.0ef7ef387191257b667314ccc401c746.js
@@ -1,27 +1,31 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9a9bc20fa85168ab9df5d0578d89e70e",
+    "revision": "f6cb017ad815541acb022fe903cbb50b",
     "url": "/portfolio-v3/index.html"
   },
   {
-    "revision": "c1579244650c255c0cac",
+    "revision": "7e44b17d13a074c3d213",
     "url": "/portfolio-v3/static/css/main.402aa2a2.chunk.css"
   },
   {
-    "revision": "594a739117e36d738cf9",
-    "url": "/portfolio-v3/static/js/2.30f889ea.chunk.js"
+    "revision": "8e8db8332c0abee258ca",
+    "url": "/portfolio-v3/static/js/2.064c6079.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/portfolio-v3/static/js/2.30f889ea.chunk.js.LICENSE.txt"
+    "url": "/portfolio-v3/static/js/2.064c6079.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c1579244650c255c0cac",
-    "url": "/portfolio-v3/static/js/main.471b830a.chunk.js"
+    "revision": "7e44b17d13a074c3d213",
+    "url": "/portfolio-v3/static/js/main.1355d71c.chunk.js"
   },
   {
     "revision": "b5eff4e927cdff1a68d8",
     "url": "/portfolio-v3/static/js/runtime-main.097b8d46.js"
+  },
+  {
+    "revision": "4164c62a7c272375e71e35f0c58d9af2",
+    "url": "/portfolio-v3/static/media/Simona-Hriscu-resume.4164c62a.pdf"
   },
   {
     "revision": "534b1a10490cdda462db7f7330a9fba3",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio-v3/precache-manifest.f7b6ce354a35709ddc825d4fce450282.js"
+  "/portfolio-v3/precache-manifest.0ef7ef387191257b667314ccc401c746.js"
 );
 
 self.addEventListener('message', (event) => {
