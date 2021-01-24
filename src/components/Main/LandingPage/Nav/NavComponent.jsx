c51500be412c8dce67a4664758caf5b06fc54fe2
SimonaHriscu <simona_hriscu@yahoo.com>
2021-01-24 23:45:49 +0100

    some fix

--- src/components/Main/LandingPage/Nav/NavComponent.jsx
+++ src/components/Main/LandingPage/Nav/NavComponent.jsx
@@ -11,20 +11,20 @@ const Nav = ({ scrollTop }) => {
   ];
   const links = ["#landing-page", "#skills", "#portfolio", "#contact"];
   const [index, setIndex] = useState({ currentIndex: 0 });
-
+  const [path, setPath] = useState(window.location.hash);
   useEffect(() => {
     if (scrollTop >= "0" && scrollTop <= "25") {
       setIndex({ currentIndex: 0 });
-      window.location.hash = links[0];
-    } else if (scrollTop <= "50") {
+      setPath((window.location.hash = links[0]));
+    } else if (scrollTop >= "26" && scrollTop <= "50") {
       setIndex({ currentIndex: 1 });
-      window.location.hash = links[1];
-    } else if (scrollTop <= "75") {
+      setPath((window.location.hash = links[1]));
+    } else if (scrollTop >= "51" && scrollTop <= "75") {
       setIndex({ currentIndex: 2 });
-      window.location.hash = links[2];
+      setPath((window.location.hash = links[2]));
     } else if (scrollTop >= "76" && scrollTop <= "100") {
       setIndex({ currentIndex: 3 });
-      window.location.hash = links[3];
+      setPath((window.location.hash = links[3]));
     }
   }, [scrollTop]);
 
