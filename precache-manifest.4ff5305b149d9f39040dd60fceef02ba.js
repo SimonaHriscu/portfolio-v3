16a558473911cec8dccadc2a5e79fbaa24e0ab4f
SimonaHriscu <simona_hriscu@yahoo.com>
2021-01-24 20:07:56 +0100

    Updates

---
+++ precache-manifest.4ff5305b149d9f39040dd60fceef02ba.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57f85d55a157b45d0a4bd0460d54d3db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4f2194195b32a97c8b4",
+    "url": "/static/css/main.20111199.chunk.css"
+  },
+  {
+    "revision": "0defb4967d142e421267",
+    "url": "/static/js/2.60687e0c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.60687e0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4f2194195b32a97c8b4",
+    "url": "/static/js/main.c158c578.chunk.js"
+  },
+  {
+    "revision": "e8bc7479af3b79cf055e",
+    "url": "/static/js/runtime-main.12159f24.js"
+  },
+  {
+    "revision": "4164c62a7c272375e71e35f0c58d9af2",
+    "url": "/static/media/Simona-Hriscu-resume.4164c62a.pdf"
+  },
+  {
+    "revision": "9944e3aaae73e3a08a817f9f46fddb54",
+    "url": "/static/media/adobexd.9944e3aa.svg"
+  },
+  {
+    "revision": "fbacd7feb7fb0830790fb37307320a07",
+    "url": "/static/media/css.fbacd7fe.svg"
+  },
+  {
+    "revision": "2fbabc7840c0a8ef3d4bb47c14fc169d",
+    "url": "/static/media/express.2fbabc78.svg"
+  },
+  {
+    "revision": "1a79a98647e7c5d0aa8a70af4f4ededf",
+    "url": "/static/media/figma.1a79a986.svg"
+  },
+  {
+    "revision": "0cb24c28f89f893c179f3d39c8d9d107",
+    "url": "/static/media/git.0cb24c28.svg"
+  },
+  {
+    "revision": "66f00eae5e0a51f3cc38016ffda80ffc",
+    "url": "/static/media/github.66f00eae.svg"
+  },
+  {
+    "revision": "3db31354b42e05f52a09785930349076",
+    "url": "/static/media/html.3db31354.svg"
+  },
+  {
+    "revision": "e1e07bdde861a3566dab7db4128e7a95",
+    "url": "/static/media/javascript.e1e07bdd.svg"
+  },
+  {
+    "revision": "d27770c188a4d08eda51b08cebe667c8",
+    "url": "/static/media/json.d27770c1.svg"
+  },
+  {
+    "revision": "534b1a10490cdda462db7f7330a9fba3",
+    "url": "/static/media/loader.534b1a10.gif"
+  },
+  {
+    "revision": "e08ed2d5bd220fba0331660930186fc0",
+    "url": "/static/media/mongodb.e08ed2d5.svg"
+  },
+  {
+    "revision": "8f85d2657215e5b03c70695d15f0f327",
+    "url": "/static/media/node.8f85d265.svg"
+  },
+  {
+    "revision": "3d25a73bde65e01f5c4a0b3fa5bf2d3a",
+    "url": "/static/media/photoshop.3d25a73b.svg"
+  },
+  {
+    "revision": "26d5318f7554647f0e7800572860b717",
+    "url": "/static/media/react native.26d5318f.svg"
+  },
+  {
+    "revision": "ebfb3a104c4ad193bf41e7eb5ccb1e29",
+    "url": "/static/media/react.ebfb3a10.svg"
+  },
+  {
+    "revision": "db086be82ed52756d712677d5af7f9fe",
+    "url": "/static/media/redux.db086be8.svg"
+  },
+  {
+    "revision": "6c5897a858cd456fa71697cebb727772",
+    "url": "/static/media/sass.6c5897a8.svg"
+  },
+  {
+    "revision": "564395c570b25dc7dee7b399e00da6bc",
+    "url": "/static/media/simona.564395c5.jpg"
+  },
+  {
+    "revision": "fcb78c5dba807a8abb13a0310f60d2eb",
+    "url": "/static/media/terminal.fcb78c5d.svg"
+  },
+  {
+    "revision": "ba526c82b455fdda70d3b6549fe09839",
+    "url": "/static/media/vsc.ba526c82.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio-v3/precache-manifest.c0d275c1348754c44d1d523ddf5ddb71.js"
+  "/precache-manifest.4ff5305b149d9f39040dd60fceef02ba.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio-v3/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
