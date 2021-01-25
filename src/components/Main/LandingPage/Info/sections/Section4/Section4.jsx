bd8caf10fa2c8a1b53c9bd4cab609c6d4170379f
Simona <see.moona@yahoo.com>
2021-01-25 19:34:47 +0100

    reload after sending form successfully

--- src/components/Main/LandingPage/Info/sections/Section4/Section4.jsx
+++ src/components/Main/LandingPage/Info/sections/Section4/Section4.jsx
@@ -1,77 +1,76 @@
 import React from 'react';
-import configData from "../config.json";
-
+import configData from '../config.json';
 
 export default class MyForm extends React.Component {
   constructor(props) {
     super(props);
     this.submitForm = this.submitForm.bind(this);
     this.state = {
-      status: ""
+      status: '',
     };
   }
 
   render() {
     const { status } = this.state;
-  return (
-    <section id={'contact'}>
-      <div className="contact-section">
-        <h1>Contact</h1>
-        <p>Let's work together! Get in touch :</p>
-        <form
-        onSubmit={this.submitForm}
-        action={`${configData.SERVER_URL}${configData.END_URL}`}
-        method="POST"
-      >
-          <label >First name</label>
-          <input type="firstName" id="firstName" required />
-          <label >Last name</label>
-          <input type="lastName" id="lastName" />
-          <label >E-mail</label>
-          <input
-            type="text"
-            name="_replyto"
-            placeholder="example@email.com"
-            required
-          />
-          <label >Message:</label>
-          <textarea
-            name="text"
-            id="textMsg"
-            rows="10"
-            required
-            placeholder="Your message here"
-          ></textarea>
-          <a href="mailto:simona_hriscu@yahoo.com?Subject=Hello">
-            <button type="submit">Send</button>
-          </a>
-          {status === "SUCCESS" ? <p>Thanks for your message!</p> :''}
-        {status === "ERROR" && <p>Ooops! There was an error.</p>}
-        </form>
-      </div>
-    </section>
-  );
+    return (
+      <section id={'contact'}>
+        <div className="contact-section">
+          <h1>Contact</h1>
+          <p>Let's work together! Get in touch :</p>
+          <form
+            onSubmit={this.submitForm}
+            action={`${configData.SERVER_URL}${configData.END_URL}`}
+            method="POST"
+          >
+            <label>First name</label>
+            <input type="firstName" id="firstName" required />
+            <label>Last name</label>
+            <input type="lastName" id="lastName" />
+            <label>E-mail</label>
+            <input
+              type="text"
+              name="_replyto"
+              placeholder="example@email.com"
+              required
+            />
+            <label>Message:</label>
+            <textarea
+              name="text"
+              id="textMsg"
+              rows="10"
+              required
+              placeholder="Your message here"
+            ></textarea>
+            <a href="mailto:simona_hriscu@yahoo.com?Subject=Hello">
+              <button type="submit">Send</button>
+            </a>
+            {status === 'SUCCESS' ? <p>Thanks for your message!</p> : ''}
+            {status === 'ERROR' && <p>Ooops! There was an error.</p>}
+          </form>
+        </div>
+      </section>
+    );
+  }
+
+  submitForm(ev) {
+    ev.preventDefault();
+    const form = ev.target;
+    const data = new FormData(form);
+    const xhr = new XMLHttpRequest();
+    xhr.open(form.method, form.action);
+    xhr.setRequestHeader('Accept', 'application/json');
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        form.reset();
+        this.setState({ status: 'SUCCESS' });
+      } else {
+        this.setState({ status: 'ERROR' });
+      }
+    };
+    xhr.send(data);
+    window.setTimeout(() => {
+      window.location.reload();
+    }, 2000);
+  }
 }
-
-
-submitForm(ev) {
-  ev.preventDefault();
-  const form = ev.target;
-  const data = new FormData(form);
-  const xhr = new XMLHttpRequest();
-  xhr.open(form.method, form.action);
-  xhr.setRequestHeader("Accept", "application/json");
-  xhr.onreadystatechange = () => {
-    if (xhr.readyState !== XMLHttpRequest.DONE) return;
-    if (xhr.status === 200) {
-      form.reset();
-      this.setState({ status: "SUCCESS" });
-    } else {
-      this.setState({ status: "ERROR" });
-    }
-  };
-  xhr.send(data);
-}
-}
-
-
